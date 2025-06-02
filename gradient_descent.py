941daa92831d03e36dc1e0ef763f87c21d5db022
Tamim Mahmud <143133639+tamim1249@users.noreply.github.com>
2025-06-02 19:00:29 +0600

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,32 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from Demos.mmapfile_demo import move_dest
 
-def gradient_descent(x, y):
-    m_curr = b_curr = 0
-    iterations = 1000
-    n = len(x)
-    learning_rate = 0.001
-    cost_list = []
 
-    for i in range(iterations):
-        y_predicted = m_curr * x + b_curr
-        cost = (1/n) * sum([val**2 for val in (y - y_predicted)])
-        cost_list.append(cost)
-        md = -(2/n) * sum(x * (y - y_predicted))
-        bd = -(2/n) * sum(y - y_predicted)
-        m_curr = m_curr - learning_rate * md
-        b_curr = b_curr - learning_rate * bd
-        print(f"m {m_curr:.4f}, b {b_curr:.4f}, cost {cost:.4f}, iteration {i}")
+def gradient_descent(x,y):
+    m_curr=b_curr=0
+    iterations=1000
+    n=len(x)
+    learning_rate=0.001
+    for i in range (iterations):
+        y_predicted=m_curr*x+b_curr
+        cost=(1/n)*sum([val**2 for val in (y-y_predicted)])
+        md=-(2/n)*sum(x*(y-y_predicted))
+        bd=-(2/n)*sum(y-y_predicted)
+        m_curr=m_curr-learning_rate*md
+        b_curr=b_curr-learning_rate*bd
+        print("m {}, b {}, cost{}, iteration {}".format(m_curr,b_curr,cost,i))
 
-    # Plotting after loop
-    plt.plot(range(iterations), cost_list)
-    plt.xlabel("Iteration")
-    plt.ylabel("Cost (MSE)")
-    plt.title("Cost vs Iteration in Gradient Descent")
-    plt.grid(True)
-    plt.show()
 
-x = np.array([1, 2, 3, 4, 5])
-y = np.array([5, 7, 9, 11, 13])
-gradient_descent(x, y)
+x=np.array([1,2,3,4,5])
+y=np.array([5,7,9,11,13])
+gradient_descent(x,y)
+
+
+
 #02/06/2025
